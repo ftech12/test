1dd263d6a65bc5d887efc83e62654553a04e7446
ftech12 <aman.negi@ftechiz.com>
2025-06-20 11:22:37 +0530

    check

--- src/pages/Assesments.tsx
+++ src/pages/Assesments.tsx
@@ -19,6 +19,7 @@ function Assesments() {
     text: 'When I feel discouraged, I recall past successes to reinforce my belief in what’s possible.',
   },
 ];
+
   return (
     <>
      <div className="max-w-2xl mx-auto p-4 space-y-6">
